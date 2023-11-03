b24c9d276e29c0251aaeb57d9795b807953518a1
root <root@virtual-linux.IBM-PoC-CSCO-Cloud-Expansion-CE.cloud>
2023-11-03 14:52:43 +0000

    2

---
+++ example-env.sh
@@ -0,0 +1,9 @@
+export HZN_AGENT_PORT=8510
+export HZN_MGMT_HUB_CERT_PATH=""
+export HZN_EXCHANGE_URL=""
+export HZN_FSS_CSSURL=""
+export HZN_AGBOT_URL=""
+export HZN_SDO_SVC_URL=""
+export HZN_FDO_SVC_URL=""
+export HZN_ORG_ID=""
+export HZN_EXCHANGE_USER_AUTH=""
